196137d20c2af212bd90686fce78a2416df210e5
Stephane <stephane_hotmail@hotmail.com>
2017-04-12 19:30:46 +0200

    Commit mais c'est pas le dernier [architecture OK, metier OK, service OK, dao OK, tests OK, front office EN COURS (authentification), fonctionnalites EN COURS]

--- ProxiBanqueSIV2/src/dao/test/DaoTest.java
+++ ProxiBanqueSIV2/src/dao/test/DaoTest.java
@@ -1,6 +1,5 @@
 package dao.test;
 
-<<<<<<< HEAD
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -31,41 +30,6 @@ public class DaoTest {
 		Adresse adresse = new Adresse("rue B", "69001", "Villeurbanne");			
 		Client client = new ClientEntreprise(1,"M","Le Gal","Vincent", adresse, "0707070707", "legalvince@aapo.fr","GTM");
 		modif.modifierClient(client); //Appel la fonction ModifierClient de DAO
-=======
-import static org.junit.Assert.*;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-import dao.Dao;
-import metier.Adresse;
-import metier.Agence;
-import metier.Client;
-
-import metier.ClientParticulier;
-import metier.ConseillerClientele;
-import service.ConseillerClienteleService;
-
-public class DaoTest {
-	
-	
-	/**
-	 * Test de la methode modifierClient dans la couche DAO.
-	 * Modification du téléphone et de l'adresse d'un même client.
-	 */
-	
-	@Test
-	public void testmodifierClient(){
-		
-		Dao modif = new Dao();
-		
-		Client clientVincentL = new ClientParticulier(1,"M","Le Gal","Vincent", new Adresse("10, Rue de la Gare", "69006", "LYON"),"0645647894","legalvince@aapo.fr");
-		
-		modif.ajouterClient(clientVincentL);
-					
-		
-		modif.modifierClient(1,"M","Le Gal","Vincent", "rue B", "69001", "Villeurbanne", "0707070707", "legalvince@aapo.fr", null); //Appel la fonction ModifierClient de DAO
->>>>>>> branch 'master' of https://github.com/serviceshuios/Proxi2-Groupe5.git
 		
 		Assert.assertEquals(true,modif.chercherClient(1).getAdresse() !=clientVincentL.getAdresse());
 		
--- ProxiBanqueSIV2/src/service/test/CompteEpargneServiceTest.java
+++ ProxiBanqueSIV2/src/service/test/CompteEpargneServiceTest.java
@@ -1,10 +1,5 @@
 package service.test;
 
-<<<<<<< HEAD
-=======
-import static org.junit.Assert.*;
-
->>>>>>> branch 'master' of https://github.com/serviceshuios/Proxi2-Groupe5.git
 import java.util.Date;
 
 import org.junit.Assert;
